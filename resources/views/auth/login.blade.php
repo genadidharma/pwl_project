37c3a6e949f2a40626c1bd82ba518e4e2cfc27a7
Genadi Dharma <genadislawa@gmail.com>
2021-05-23 13:16:08 +0800

    autentikasi & otorisasi - membuat fitur reset password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,84 +1,71 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.auth')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/vendors/bootstrap-icons/bootstrap-icons.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/css/app.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/css/pages/auth.css')}}">
-</head>
+@section('title', 'Login')
 
-<body>
-
-    <div id="auth">
-
-        <div class="row h-100">
-            <div class="col-lg-5 col-12">
-                <div id="auth-left">
-                    <div class="auth-logo">
-                        <a href="index.html"><img src="assets/images/logo/logo.png" alt="Logo"></a>
-                    </div>
-                    <h1 class="auth-title">Log in.</h1>
-                    <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p>
-
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-                        <div class="form-group position-relative has-icon-left mb-4">
-                            <input id="username" type="username" class="form-control form-control-xl @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" placeholder="Username" autofocus>
-                            <div class="form-control-icon">
-                                <i class="bi bi-person"></i>
-                            </div>
-
-                            @error('username')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                        <div class="form-group position-relative has-icon-left mb-4">
-                            <input id="password" type="password" class="form-control form-control-xl @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
-                            <div class="form-control-icon">
-                                <i class="bi bi-shield-lock"></i>
-                            </div>
-
-                            @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                        <div class="form-check form-check-lg d-flex align-items-end">
-                            <div class="form-check">
-                                <input class="form-check-input me-2" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                <label class="form-check-label text-gray-600" for="remember">
-                                    Ingat Saya
-                                </label>
-                            </div>
-                        </div>
-                        <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
-                    </form>
-                    <div class="text-center mt-5 text-lg fs-4">
-                        @if (Route::has('password.request'))
-                        <a class="font-bold" href="{{ route('password.request') }}">
-                            Lupa Password?
-                        </a>
-                        @endif
-                    </div>
-                </div>
+@section('content')
+<div class="row h-100">
+    <div class="col-lg-5 col-12">
+        <div id="auth-left">
+            <div class="auth-logo">
+                <img src="/assets/images/logo/logo.png" alt="Logo">
             </div>
-            <div class="col-lg-7 d-none d-lg-block">
-                <div id="auth-right">
+            <h1 class="auth-title">Log in.</h1>
+            <p class="auth-subtitle mb-5">Log in ke Pethouse</p>
 
+            @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
+            @endif
+            
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="form-group position-relative has-icon-start mb-4  @error('username') mb-5 @enderror">
+                    <input id="username" type="username" class="form-control form-control-xl @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" placeholder="Username" autofocus>
+                    <div class="form-control-icon">
+                        <i class="bi bi-person"></i>
+                    </div>
+
+                    @error('username')
+                    <span class="invalid-feedback position-absolute" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
+                <div class="form-group position-relative has-icon-start mb-4 @error('password') mb-5 @enderror">
+                    <input id="password" type="password" class="form-control form-control-xl @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
+                    <div class="form-control-icon">
+                        <i class="bi bi-shield-lock"></i>
+                    </div>
+
+                    @error('password')
+                    <span class="invalid-feedback position-absolute" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <div class="form-check form-check-lg d-flex align-items-end">
+                    <input class="form-check-input me-2" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                    <label class="form-check-label text-gray-600" for="remember">
+                        Ingat Saya
+                    </label>
+                </div>
+                <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
+            </form>
+            <div class="text-center mt-5 text-lg fs-4">
+                @if (Route::has('password.request'))
+                <a class="font-bold" href="{{ route('password.request') }}">
+                    Lupa Password?
+                </a>
+                @endif
             </div>
         </div>
-
     </div>
-</body>
+    <div class="col-lg-7 d-none d-lg-block">
+        <div id="auth-right">
 
-</html>
+        </div>
+    </div>
+</div>
+@endsection
